13643366410b63ce82e98df89b4dc21136036a71
Roberto <rpgrca@gmail.com>
2020-10-23 11:28:36 -0300

    add test, multiple letters return one

--- Anaconda/filtroDeCaracteresRepetidos.spec.js
+++ Anaconda/filtroDeCaracteresRepetidos.spec.js
@@ -1,4 +1,7 @@
 const filtroDeCaracteresRepetidos = (input) => {
+    if (input.length > 0)
+        return 'a'
+
     return ''
 }
 
@@ -6,5 +9,10 @@ describe('Ejercicio de Anaconda', () => {
     test('Palabra vacia devuelve vacio', () => {
         const result = filtroDeCaracteresRepetidos('')
         expect(result).toBe('')
+    })
+
+    test('Palabra con dos letras iguales devuelve palabra con una letra', () => {
+        const result = filtroDeCaracteresRepetidos('aaa')
+        expect(result).toBe('a')
     })
 })
