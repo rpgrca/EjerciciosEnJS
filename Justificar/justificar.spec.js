802bfa6c93b3cb10b666560920e59d6e5a0b08e2
Roberto <rpgrca@gmail.com>
2020-11-09 10:53:06 -0300

    add test, length >= cadena

--- Justificar/justificar.spec.js
+++ Justificar/justificar.spec.js
@@ -5,4 +5,9 @@ describe('Ejercicio de Justificacion', () => {
         const result = justificar('a', 0)
         expect(result).toBe('a')
     })
+
+    test.each([6,10])('Longitud mayor o igual a longitud de cadena (%i) devuelve cadena', (n) => {
+        const result = justificar('cadena', n)
+        expect(result).toBe('cadena')
+    })
 })
