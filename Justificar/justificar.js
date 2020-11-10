cd514f744d830fed89c1d1b6c6e143cb38f5abd7
Roberto <rpgrca@gmail.com>
2020-11-10 16:20:46 -0300

    add test, split when ending perfectly

--- Justificar/justificar.js
+++ Justificar/justificar.js
@@ -1,3 +1,7 @@
 export const justificar = (input, longitud) => {
-    return input;
+    if (longitud < 1) return [ input ]
+
+    var regex = new RegExp(`.{1,${longitud}}`, 'g')
+    return input.match(regex)
+                .map(Function.prototype.call, String.prototype.trim)
 }
--- Justificar/justificar.spec.js
+++ Justificar/justificar.spec.js
@@ -3,11 +3,16 @@ import { justificar } from './justificar'
 describe('Ejercicio de Justificacion', () => {
     test('Longitud invalida devuelve cadena original', () => {
         const result = justificar('a', 0)
-        expect(result).toBe('a')
+        expect(result).toStrictEqual([ 'a' ])
     })
 
     test.each([6,10])('Longitud mayor o igual a longitud de cadena (%i) devuelve cadena', (n) => {
         const result = justificar('cadena', n)
-        expect(result).toBe('cadena')
+        expect(result).toStrictEqual([ 'cadena' ])
+    })
+
+    test('Longitud de cadena mayor a longitud y palabra termina justo justifica primer parte', () => {
+        const result = justificar('Esta es una cadena de muchos caracteres', 21)
+        expect(result).toStrictEqual([ 'Esta es una cadena de', 'muchos caracteres' ])
     })
 })
