031e3d098a181857335c4a5c3935b40d35cc44fb
Roberto <rpgrca@gmail.com>
2020-11-10 23:46:28 -0300

    add test, justify correctly when lines are smaller

--- Justificar/justificar.js
+++ Justificar/justificar.js
@@ -1,7 +1,40 @@
 export const justificar = (input, longitud) => {
     if (longitud < 1) return [ input ]
 
-    var regex = new RegExp(`(.{1,${longitud}})[ ]*`, 'g')
-    return input.match(regex)
-                .map(Function.prototype.call, String.prototype.trim)
+    var lineas = input.split(' ').reduce(function(lineas, palabra) {
+        var texto_anterior = lineas[lineas.length - 1].join('')
+        var texto = texto_anterior
+        if (texto.length != 0) {
+            texto = texto.concat(" ")
+        }
+
+        texto = texto.concat(palabra)
+        if (texto.length <= longitud) {
+            if (lineas[lineas.length - 1].length != 0) {
+                lineas[lineas.length - 1] = [...lineas[lineas.length - 1], ' ', palabra]
+            }
+            else {
+                lineas[lineas.length - 1].push([palabra])
+            }
+        }
+        else {
+            var index = 1
+            var espacio = longitud - texto_anterior.length
+            while (espacio-- > 0)
+            {
+                lineas[lineas.length - 1][index] = lineas[lineas.length - 1][index].concat(' ')
+                index += 2
+                if (index >= lineas[lineas.length - 1].length)
+                {
+                    index = 1
+                }
+            }
+
+            lineas.push([palabra])
+        }
+
+        return lineas
+    }, [[]])
+
+    return lineas.map((linea) => linea.join('').trim())
 }
--- Justificar/justificar.spec.js
+++ Justificar/justificar.spec.js
@@ -13,11 +13,21 @@ describe('Ejercicio de Justificacion', () => {
 
     test('Longitud de cadena mayor a longitud y palabra termina justo solamente debe dividir', () => {
         const result = justificar('Esta es una cadena de muchos caracteres', 21)
-        expect(result).toStrictEqual([ 'Esta es una cadena de', 'muchos caracteres' ])
+        expect(result).toStrictEqual([ 'Esta es una cadena de',
+                                       'muchos caracteres' ])
     })
 
     test('Con multiples lineas que terminan correctamente solamente debe dividir', () => {
         const result = justificar('Esta otra cadena es bastante parecida a la cadena anterior pero con tres lineas bien escritas', 39)
-        expect(result).toStrictEqual( [ 'Esta otra cadena es bastante parecida a', 'la cadena anterior pero con tres lineas', 'bien escritas'])
+        expect(result).toStrictEqual([ 'Esta otra cadena es bastante parecida a',
+                                       'la cadena anterior pero con tres lineas',
+                                       'bien escritas' ])
+    })
+
+    test('Con multiples lineas que no terminan bien debe dividir y justificar', () => {
+        const result = justificar('Esta es una cadena de varios caracteres que deberia ser justificada.', 30)
+        expect(result).toStrictEqual([ 'Esta  es  una cadena de varios',
+                                       'caracteres   que  deberia  ser',
+                                       'justificada.'])
     })
 })
