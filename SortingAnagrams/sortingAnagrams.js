4846322b2017649bd552c52486c1d356692dd9d2
Roberto <rpgrca@gmail.com>
2021-04-29 10:30:25 -0300

    add test: return same list if no anagrams in it

--- SortingAnagrams/sortingAnagrams.js
+++ SortingAnagrams/sortingAnagrams.js
@@ -1,3 +1,7 @@
 export const isAnagramOf = (firstWord, secondWord) => {
     return firstWord.split('').sort().join() == secondWord.split('').sort().join();
 }
+
+export const removeAnagramsFrom = (words) => {
+    return words;
+}
--- SortingAnagrams/sortingAnagrams.spec.js
+++ SortingAnagrams/sortingAnagrams.spec.js
@@ -1,4 +1,4 @@
-import { isAnagramOf } from './sortingAnagrams'
+import { isAnagramOf, removeAnagramsFrom } from './sortingAnagrams'
 
 describe('Ejercicio de Anagrama', () => {
     test('Palabras vacias devuelve verdadero', () => {
@@ -14,5 +14,13 @@ describe('Ejercicio de Anagrama', () => {
     test('Anagramas devuelve verdadero', () => {
         const result = isAnagramOf('code', 'doce')
         expect(result).toBe(true)
+    })
+})
+
+describe('Ejercicio de Eliminacion', () => {
+    test('Si no hay anagramas en la lista retorna la misma lista', () => {
+        const input = ['code', 'frame']
+        const result = removeAnagramsFrom(input)
+        expect(result).toBe(input)
     })
 })
