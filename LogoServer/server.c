81af1d121300cecf944b72455323abc4b4fa9257
Roberto <rpgrca@gmail.com>
2022-03-27 22:59:38 -0300

    clean up, simplify code

--- LogoServer/server.c
+++ LogoServer/server.c
@@ -41,67 +41,66 @@ static char *_footer = "╚═════════════════�
 static enum Movement _direction = Top;
 static enum Mode _mode = Draw;
 
+#define HEADER_SIZE ((32 * 3) + 2)
+#define FOOTER_SIZE ((32 * 3) + 4)
+
+static bool try_send(int s, char *buffer, size_t length) {
+	if (send(s, buffer, length, 0) == -1) {
+		perror("send() failed");
+		return false;
+	}
+
+	return true;
+}
+
 bool send_handshake(int s) {
     char *handshake_message = "hello\r\n";
 
-	if (send(s, handshake_message, strlen(handshake_message), 0) == -1) {
-		perror("send() failed");
-		return false;
-	}
-
-	return true;
+	return try_send(s, handshake_message, strlen(handshake_message));
 }
 
-bool build_coordinates(int s) {
-	char buffer[1024];
+static bool build_coordinates(int s) {
+	char buffer[16];
 	snprintf(buffer, sizeof(buffer), "(%d,%d)\r\n", _x, _y);
 
-	if (send(s, buffer, strlen(buffer), 0) == -1) {
-		perror("send() failed");
-		return false;
-	}
-
-	return true;
+	return try_send(s, buffer, strlen(buffer));
 }
 
-bool renderize(int s) {
-	char buffer[2048];
+static bool renderize(int s) {
+	char buffer[1400];
+	int current = HEADER_SIZE;
 
 	memset(buffer, 0, sizeof(buffer));
-	memcpy(buffer, _header, 32 * 3 + 2);  // 98
+	memcpy(buffer, _header, HEADER_SIZE);
 	for (int i = 0; i < HEIGHT; i++) {
-		char line[256];
-		memset(line, 0, sizeof(line));
-		memcpy(line, "║", 3);          //  3
-		memcpy(&line[3], _map[i], 30); // 33
-		memcpy(&line[33], "║", 3);     // 36
-		memcpy(&line[36], "\r\n", 2);  // 38
-
-		memcpy(&buffer[98 + (38 * i)], line, 38);
+		memcpy(&buffer[current], "║", 3);
+		current += 3;
+		memcpy(&buffer[current], _map[i], 30);
+		current += 30;
+		memcpy(&buffer[current], "║", 3);
+		current += 3;
+		memcpy(&buffer[current], "\r\n", 2);
+		current += 2;
 	}
 
-	memcpy(&buffer[1238], _footer, 32 * 3 + 4);
+	memcpy(&buffer[current], _footer, FOOTER_SIZE);
+	current += FOOTER_SIZE;
 
-	if (send(s, buffer, 1338, 0) == -1) {
-		perror("send() failed");
-		return false;
-	}
-
-	return true;
+	return try_send(s, buffer, current);
 }
 
-void move_to(int new_x, int new_y) {
+static void move_to(int new_x, int new_y) {
 	switch (_mode) {
 		case Draw: _map[_y][_x] = '*'; break;
 		case Eraser: _map[_y][_x] = ' '; break;
-		case Hover: break;
+		case Hover: /* just move */ break;
 	}
 
 	_x = new_x;
 	_y = new_y;
 }
 
-void step(int steps) {
+void advance(int steps) {
 	for (int i = 0; i < steps; i++) {
 		switch (_direction) {
 			case Top: if (_y > 0) move_to(_x, _y - 1); break;
@@ -116,10 +115,9 @@ void step(int steps) {
 	}
 }
 
-static void right(int steps) {
+static void turn_right(int steps) {
 	_direction = (_direction + steps) % 8;
 }
-
 
 static void reset_mode() {
 	_x = _y = 15;
@@ -128,9 +126,7 @@ static void reset_mode() {
 }
 
 static void reset_map() {
-	for (int y = 0; y < HEIGHT; y++)
-		for (int x = 0; x < WIDTH; x++)
-			_map[y][x] = ' ';
+	memset(_map, ' ', sizeof(_map));
 }
 
 static bool process_message(char *buffer, int s) {
@@ -188,7 +184,7 @@ static bool process_message(char *buffer, int s) {
 				if (strncmp(&buffer[index], "left", 4) == 0) {
 					int n = atoi(&buffer[index + 4]);
 					if (n == 0) n = 1;
-					right(8 - n);
+					turn_right(8 - n);
 
 					index += 4;
 					while (index < length && buffer[index] != '\r')
@@ -215,7 +211,7 @@ static bool process_message(char *buffer, int s) {
 					if (strncmp(&buffer[index], "right", 5) == 0) {
 						int n = atoi(&buffer[index + 6]);
 						if (n == 0) n = 1;
-						right(n);
+						turn_right(n);
 
 						index += 5;
 						while (index < length && buffer[index] != '\r')
@@ -228,7 +224,7 @@ static bool process_message(char *buffer, int s) {
 			case 's':
 				if (strncmp(&buffer[index], "steps ", 6) == 0) {
 					int n = atoi(&buffer[index + 6]);
-					step(n);
+					advance(n);
 
 					index += 6;
 					while (index < length && buffer[index] != '\r')
@@ -246,10 +242,20 @@ static bool process_message(char *buffer, int s) {
 	return true;
 }
 
+static bool bind_socket_to_ip(int s) {
+    struct sockaddr_in addr;
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(SERVER_PORT);
+
+    return bind(s, (struct sockaddr *)&addr, sizeof(addr)) != -1;
+}
+
 int main() {
     char buffer[1024];
     int s;
-	int closing_descriptor = false;
 
     s = socket(AF_INET, SOCK_STREAM, 0);
     if (s == -1) {
@@ -257,17 +263,11 @@ int main() {
         exit(-1);
     }
 
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(SERVER_PORT);
-
-    if (bind(s, (struct sockaddr *)&addr, sizeof(addr))) {
-        perror("bind() failed");
-        close(s);
-        exit(-1);
-    }
+	if (! bind_socket_to_ip(s)) {
+		perror("bind() failed");
+		close(s);
+		exit (-1);
+	}
 
     if (listen(s, 5) == -1) {
         perror("listen() failed");
@@ -276,38 +276,31 @@ int main() {
     }
 
 	printf("Waiting connections at port %d...\n", SERVER_PORT);
-
 	while (true) {
 	    int new_s = accept(s, NULL, NULL);
 
 	    if (new_s == -1) {
-	        if (errno != EWOULDBLOCK) {
-	            perror("accept() failed");
-				close(s);
-				exit(-1);
-	        }
+            perror("accept() failed");
+			close(s);
+			exit(-1);
 		}
 
 		reset_map();
 		reset_mode();
+
 		if (send_handshake(new_s)) {
 			while (true) {
 	            memset(buffer, 0, sizeof(buffer));
 	            int bytes = recv(new_s, buffer, sizeof(buffer), 0);
 	            if (bytes == -1) {
-	                if (errno != EWOULDBLOCK) {
-	                    perror("recv() failed");
-	                    close(new_s);
-	                }
+					perror("recv() failed");
+	                close(new_s);
 
 					break;
 	            }
 
-				closing_descriptor = !process_message(buffer, new_s);
-
-				if (closing_descriptor) {
+				if (! process_message(buffer, new_s))
 					break;
-		        }
 			}
 
 			close(new_s);
